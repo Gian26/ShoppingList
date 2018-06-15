732b59de881200d09198a026dc24d1f09631a5c3
Gian26 <giancuello26@gmail.com>
2018-06-15 02:59:01 -0500

    add menu functionality and dev tool options. Created window to add items to list

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const path = require('path');
 const {app,BrowserWindow,Menu }= electron;
 
 let mainWindow;
-
+let addWindow;
 
 // listen for the app to be ready,
 
@@ -18,18 +18,48 @@ app.on('ready', function(){
     protocol:'file:',
     slashes:true
   }));
+
+  //Quit app when closed
+  mainWindow.on('closed',function(){
+    app.quit();
+  });
+
   //build menu from template
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
   //Insert the Menu
   Menu.setApplicationMenu(mainMenu);
 });
 
+
+//Handle create add window
+function createAddWindow(){
+  // create new mainWindow
+  addWindow = new BrowserWindow({
+    width:300,
+    height:200,
+    title:'Add Shopping list item'
+  });
+  // load html file into Window
+  addWindow.loadURL(url.format({
+    pathname:path.join(__dirname, 'addWindow.html'),
+    protocol:'file:',
+    slashes:true
+  }));
+  //Garbage Collection
+  addWindow.on('closed',function(){
+    addWindow=null;
+  });
+}
+
 //create menu template
 const mainMenuTemplate = [
   {
     label:'File',
     submenu:[{
-      label:'Add Item'
+      label:'Add Item',
+      click(){
+        createAddWindow();
+      }
     },
     {
       label:'Clear Items'
@@ -44,3 +74,28 @@ const mainMenuTemplate = [
   ]
   }
 ];
+
+//if mac add empty object to menuTemplate
+if(process.platform=='darwin'){
+  mainMenuTemplate.unshift({});//adding it to the beggining of the array
+}
+
+
+//Add Developer tools item if not in prod
+if(process.env.NODE_ENV != 'production'){
+  mainMenuTemplate.push({
+    label:'Developer Tools',
+    submenu:[
+      {
+        label:'Toggle DevTools',
+        accelerator:process.platform=='darwin'?'Command+I':'Ctrl+I',
+        click(item,focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role:'reload'
+      }
+    ]
+  });
+}
